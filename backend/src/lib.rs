d3e84583448971bda7c250a793892d3b1bd9da0b
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2023-04-03 19:21:22 +0700

    initial commit

---
+++ backend/src/lib.rs
@@ -0,0 +1,24 @@
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router};
+
+mod telemetry;
+
+#[tokio::main]
+pub async fn start_server() {
+    let subscriber = telemetry::get_subscriber("backend".into(), "info".into(), std::io::stdout);
+    telemetry::init_subscriber(subscriber);
+
+    let app = Router::new().route("/", get(root));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::info!("server listening on {addr}");
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    backend::start_server();
+}
