1a58a04bda350d562183a43adb5f621b8a2b8cc8
Tigor Hutasuhut <tigor.hutasuhut@gmail.com>
2023-04-06 16:50:58 +0700

    feat(svelte): added flowbite

--- www/src/App.svelte
+++ www/src/App.svelte
@@ -1,51 +1,31 @@
-<style>
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-}
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-}
-.read-the-docs {
-    color: #888;
-}
-</style>
-
 <script lang="ts">
-import svelteLogo from "./assets/svelte.svg";
-import viteLogo from "/vite.svg";
-import Counter from "./lib/Counter.svelte";
+import { Alert } from "flowbite-svelte";
+import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
 </script>
 
 <main>
-    <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-            <img src={viteLogo} class="logo" alt="Vite Logo" />
-        </a>
-        <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-            <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-        </a>
-    </div>
-    <h1>Vite + Svelte</h1>
-
-    <div class="card">
-        <Counter />
-    </div>
-
-    <p>
-        Check out <a
-            href="https://github.com/sveltejs/kit#readme"
-            target="_blank"
-            rel="noreferrer">SvelteKit</a
-        >, the official Svelte app framework powered by Vite!
-    </p>
-
-    <p class="read-the-docs">
-        Click on the Vite and Svelte logos to learn more
-    </p>
+    <Navbar let:hidden let:toggle>
+        <NavBrand href="/">
+            <img
+                src="https://flowbite.com/docs/images/logo.svg"
+                class="mr-3 h-6 sm:h-9"
+                alt="Flowbite Logo" />
+            <span
+                class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+                Whitea
+            </span>
+        </NavBrand>
+        <NavHamburger on:click={toggle} />
+        <NavUl hidden={hidden}>
+            <NavLi href="/" active={true}>Home</NavLi>
+            <NavLi href="/about">About</NavLi>
+            <NavLi href="/services">Services</NavLi>
+            <NavLi href="/pricing">Pricing</NavLi>
+            <NavLi href="/contact">Contact</NavLi>
+        </NavUl>
+    </Navbar>
+    <Alert>
+        <span class="font-bold">Info alert!</span> Change a few things up and try
+        submitting again.
+    </Alert>
 </main>
--- www/svelte.config.js
+++ www/svelte.config.js
@@ -2,12 +2,12 @@ import preprocess from "svelte-preprocess";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: [
-    vitePreprocess(),
-    preprocess({
-      postcss: true,
-    }),
-  ],
+    // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+    // for more information about preprocessors
+    preprocess: [
+        vitePreprocess(),
+        preprocess({
+            postcss: true,
+        }),
+    ],
 };
